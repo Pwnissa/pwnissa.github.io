4c0ca2bb73b9418b6e0f3e22189a8836458e496c
stealth <alessandriagabriele@gmail.com>
2025-07-11 16:29:15 +0200

    navbar links from costants

---
+++ project/src/consts.ts
@@ -0,0 +1,10 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Pwnissa - CTF Team';
+export const BLOG_TITLE = 'BLOG Pwnissa - CTF Team';
+export const SITE_DESCRIPTION = '';
+export const navLinks = [
+    { name: "Blog", href: "/blog" },
+    { name: "Our Pets", href: "/our-pets" },
+]
