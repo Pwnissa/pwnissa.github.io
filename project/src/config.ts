61bbad898553c07e7fdd9783ffe99ad4b2394882
David <dhermes.mail@web.de>
2025-07-26 21:37:48 +0200

    added config file

---
+++ project/src/config.ts
@@ -0,0 +1,146 @@
+
+export const ctfLogos = [
+  { name: "ENOWARS 9 - 2025", 
+    img_url: "https://ctftime.org/media/cache/04/2e/042e05ae002396dff908a7474c408ab1.png", 
+    platform_link:  "https://ctftime.org/event/2796"},
+  { name: "CyberChallenge.it Nationals", 
+    img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrOiiSt0riNNROJCDmj_9TWL4lDXh04-JHOA&s", 
+    platform_link:  "https://cyberchallenge.it/attack-defense/2025"},
+  { name: "CornCTF 2025", 
+    img_url: "https://ctftime.org/media/events/cornctf.png",
+    platform_link: "https://ctftime.org/event/2762"},
+  { name: "LeakCTF 2025", 
+    img_url: "https://ctftime.org/media/events/L3akCTF_2025_Logo_750x750.png",
+    platform_link: "https://ctftime.org/event/2629"},
+  { name: "HackTheBox", img_url: "https://yt3.googleusercontent.com/2Tq9apgiHSV7NrCKzgRMbm-AABWJJXuyWMY_7MpQlVdvemqJRWcGegVq0G4e9xHCl8HcbQs_Ag=s900-c-k-c0x00ffffff-no-rj" },
+  { name: "TryHackMe: Industrial Intrusion", img_url: "https://appl3tree.github.io/assets/img/infocards/thm-industrial-ctf.png" },
+];
+
+export const members = [
+  {
+    nickname: "StealthGuy",
+    name: "Gabriele Alessandria",
+    photo: "/immagini_membri/gabriele_alessandria.jpg",
+    link: "https://stealthguy.net",
+    tags: ["web", "network", "misc"]
+  },
+  {
+    nickname: "Ub0k",
+    name: "David Hermes",
+    photo: "/immagini_membri/david_hermes.jpg",
+    link: "https://github.com/-1xUb1k",
+    tags: ["pwn", "reverse"]
+  },
+  {
+    nickname: "AndyVale",
+    name: "Andrea Valentino Ricotti",
+    photo: "/immagini_membri/andrea_ricotti.jpg",
+    link: "https://github.com/AndyVale",
+    tags: ["crypto"]
+  },
+  {
+    nickname: "ThePirate",
+    name: "Pietro Zignaigo",
+    photo: "/immagini_membri/pietro_zignaigo.jpg",
+    link: "https://github.com/ThePirate41",
+    tags: ["crypto", "reverse"]
+  },
+  {
+    nickname: "Redistan",
+    name: "Riccardo Cherchi",
+    photo: "/immagini_membri/riccardo_cherchi.jpg",
+    link: "https://www.linkedin.com/in/riccardo-cherchi-2848b0181/",
+    tags: ["web", "misc"]
+  },
+  {
+    nickname: "Goligx",
+    name: "Gabriele 1",
+    photo: "/immagini_membri/gabriele_olivieri.jpg",
+    link: "https://github.com/Goligx",
+    tags: ["crypto", "misc"]
+  }, 
+  {
+    nickname: "SGDav",
+    name: "Davide Caldirola",
+    photo: "/immagini_membri/davide_caldirola.jpg",
+    link: "https://github.com/DavideCaldirola/CTF-Writeups",
+    tags: ["web","misc"]
+  },
+  {
+    nickname: "marco",
+    name: "Marco Pellero",
+    photo: "/immagini_membri/marco_pellero.jpg",
+    link: "https://www.github.com/MarcoPellero",
+    tags: ["pwn"]
+  },
+  {
+    nickname: "gerert",
+    name: "Giovanni B. Aragone",
+    photo: "/immagini_membri/giovanni_aragone.jpg",
+    link: "https://www.google.com",
+    tags: ["misc" , "web"]
+  },
+  {
+    nickname: "eriseisarai",
+    name: "Erika",
+    photo: "/immagini_membri/erika_corbo.jpg",
+    link: "https://github.com/eriseisarai",
+    tags: ["misc", "web"]
+  },
+  {
+    nickname: "Tom1379",
+    name: "Tommaso Thea Cioni",
+    photo: "https://images.pexels.com/photos/1043470/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=300&h=300&fit=crop",
+    link: "https://www.google.com",
+    tags: ["pwn"]
+  },
+  {
+    nickname: "rikyiso00",
+    name: "Riccardo Isola",
+    photo: "/immagini_membri/riccardo_isola.jpg",
+    link: "https://www.google.com",
+    tags: ["Web"]
+  },
+  {
+    nickname: "il_vero_gabriele",
+    name: "[REDACTED]",
+    photo: "https://static.wikia.nocookie.net/spongebob/images/f/fc/SpongeBob_SquarePants_Theme_Song_%281998%29_01.png",
+    link: "https://www.google.com",
+    tags: ["pwn"]
+  },
+  {
+    nickname: "Pwny",
+    name: "Pwny the Horse",
+    photo: "/immagini_membri/pwny.jpg",
+    link: "https://github.com/WAPEETY/ASCII-horses",
+    tags: ["pwn", "gambling"]
+  },
+];
+
+export const pets = [
+  {
+    name: "Lupina",
+    info: "In loving memory of Lupina, forever in my heart.",
+    photo: "/img-pets/crop_lupina.jpeg", // Make sure to have these image paths
+    tags: ["Dog"],
+  },
+  {
+    name: "Tofu",
+    info: "Cat",
+    photo: "/images/pets/luna.jpg",
+    tags: ["Cat"],
+  },
+
+  {
+    name: "B3n1t0",
+    info: "A leopard gecko that loves to hang upside down in its free time",
+    photo: "/img-pets/B3n1t0.jpg",
+    tags: ["Reptile", "Exotic"],
+  },
+  {
+    name: "Macchia",
+    info: "Why does he sit like that? Nobody knows",
+    photo: "/img-pets/macchia.jpg",
+    tags: ["Cat"],
+  }
+];
