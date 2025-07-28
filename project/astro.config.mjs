ff9726af4a4914471913f5043ae22b131d897449
David <dhermes.mail@web.de>
2025-07-28 16:20:24 +0200

    ahhhh

--- project/astro.config.mjs
+++ project/astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/',
+  base: '/pwnissa.github.io/',
   integrations: [mdx()],
   markdown: {
     drafts: true,
