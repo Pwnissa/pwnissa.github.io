5bf8e4765e9810c3abab1a09c78beaa753959398
Riccardo Cherchi <richicherchi@gmail.com>
2025-10-22 23:40:23 +0200

    Added new member

--- project/src/config.ts
+++ project/src/config.ts
@@ -1,22 +1,42 @@
-
 export const ctfLogos = [
-  { name: "UIUCTF 2025", 
-    img_url: "https://ctftime.org/media/cache/8f/44/8f44cc2cb38ce3a8be121f45d521d902.png",
-    platform_link: "https://ctftime.org/event/2640"},
-  { name: "ENOWARS 9 - 2025", 
-    img_url: "https://ctftime.org/media/cache/04/2e/042e05ae002396dff908a7474c408ab1.png", 
-    platform_link:  "https://ctftime.org/event/2796"},
-  { name: "CyberChallenge.it Nationals", 
-    img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrOiiSt0riNNROJCDmj_9TWL4lDXh04-JHOA&s", 
-    platform_link:  "https://cyberchallenge.it/attack-defense/2025"},
-  { name: "CornCTF 2025", 
+  {
+    name: "UIUCTF 2025",
+    img_url:
+      "https://ctftime.org/media/cache/8f/44/8f44cc2cb38ce3a8be121f45d521d902.png",
+    platform_link: "https://ctftime.org/event/2640",
+  },
+  {
+    name: "ENOWARS 9 - 2025",
+    img_url:
+      "https://ctftime.org/media/cache/04/2e/042e05ae002396dff908a7474c408ab1.png",
+    platform_link: "https://ctftime.org/event/2796",
+  },
+  {
+    name: "CyberChallenge.it Nationals",
+    img_url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrOiiSt0riNNROJCDmj_9TWL4lDXh04-JHOA&s",
+    platform_link: "https://cyberchallenge.it/attack-defense/2025",
+  },
+  {
+    name: "CornCTF 2025",
     img_url: "https://ctftime.org/media/events/cornctf.png",
-    platform_link: "https://ctftime.org/event/2762"},
-  { name: "LeakCTF 2025", 
+    platform_link: "https://ctftime.org/event/2762",
+  },
+  {
+    name: "LeakCTF 2025",
     img_url: "https://ctftime.org/media/events/L3akCTF_2025_Logo_750x750.png",
-    platform_link: "https://ctftime.org/event/2629"},
-  { name: "HackTheBox", img_url: "https://yt3.googleusercontent.com/2Tq9apgiHSV7NrCKzgRMbm-AABWJJXuyWMY_7MpQlVdvemqJRWcGegVq0G4e9xHCl8HcbQs_Ag=s900-c-k-c0x00ffffff-no-rj" },
-  { name: "TryHackMe: Industrial Intrusion", img_url: "https://appl3tree.github.io/assets/img/infocards/thm-industrial-ctf.png" },
+    platform_link: "https://ctftime.org/event/2629",
+  },
+  {
+    name: "HackTheBox",
+    img_url:
+      "https://yt3.googleusercontent.com/2Tq9apgiHSV7NrCKzgRMbm-AABWJJXuyWMY_7MpQlVdvemqJRWcGegVq0G4e9xHCl8HcbQs_Ag=s900-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    name: "TryHackMe: Industrial Intrusion",
+    img_url:
+      "https://appl3tree.github.io/assets/img/infocards/thm-industrial-ctf.png",
+  },
 ];
 
 export const members = [
@@ -25,98 +45,107 @@ export const members = [
     name: "Gabriele Alessandria",
     photo: "/immagini_membri/gabriele_alessandria.jpg",
     link: "https://stealthguy.net",
-    tags: ["web", "network", "misc"]
+    tags: ["web", "network", "misc"],
   },
   {
     nickname: "Ubik",
     name: "David Hermes",
     photo: "/immagini_membri/david_hermes.jpg",
     link: "https://www.linkedin.com/in/david-hermes-46168226a/",
-    tags: ["pwn", "reverse"]
+    tags: ["pwn", "reverse"],
   },
   {
     nickname: "AndyVale",
     name: "Andrea Valentino Ricotti",
     photo: "/immagini_membri/andrea_ricotti.jpg",
     link: "https://github.com/AndyVale",
-    tags: ["crypto"]
+    tags: ["crypto"],
   },
   {
     nickname: "ThePirate",
     name: "Pietro Zignaigo",
     photo: "/immagini_membri/pietro_zignaigo.jpg",
     link: "https://github.com/ThePirate42",
-    tags: ["crypto", "reverse"]
+    tags: ["crypto", "reverse"],
   },
   {
     nickname: "Redistan",
     name: "Riccardo Cherchi",
     photo: "/immagini_membri/riccardo_cherchi.jpg",
     link: "https://www.linkedin.com/in/riccardo-cherchi-2848b0181/",
-    tags: ["web", "misc"]
+    tags: ["web", "misc"],
+  },
+  {
+    nickname: "e3py",
+    name: "Giacomo Quarto",
+    photo: "/immagini_membri/giacomo_quarto.jpg",
+    link: "https://github.com/lmvjack",
+    tags: ["web", "misc"],
   },
   {
     nickname: "Goligx",
     name: "Gabriele Olivieri",
     photo: "/immagini_membri/gabriele_olivieri.jpg",
     link: "https://github.com/Goligx",
-    tags: ["crypto", "misc"]
-  }, 
+    tags: ["crypto", "misc"],
+  },
   {
     nickname: "SGDav",
     name: "Davide Caldirola",
     photo: "/immagini_membri/davide_caldirola.jpg",
     link: "https://github.com/DavideCaldirola/CTF-Writeups",
-    tags: ["web","misc"]
+    tags: ["web", "misc"],
   },
   {
     nickname: "marco",
     name: "Marco Pellero",
     photo: "/immagini_membri/marco_pellero.jpg",
     link: "https://www.github.com/MarcoPellero",
-    tags: ["pwn"]
+    tags: ["pwn"],
   },
   {
     nickname: "gerert",
     name: "Giovanni B. Aragone",
     photo: "/immagini_membri/giovanni_aragone.jpg",
     link: "",
-    tags: ["misc" , "web"]
+    tags: ["misc", "web"],
   },
   {
     nickname: "eriseisarai",
     name: "Erika Corbo",
     photo: "/immagini_membri/erika_corbo.jpg",
     link: "https://github.com/eriseisarai",
-    tags: ["misc", "web"]
+    tags: ["misc", "web"],
   },
   {
     nickname: "Tom1379",
     name: "Tommaso Thea Cioni",
-    photo: "https://i.pinimg.com/736x/2f/02/59/2f02592195355b96dc35480cf6a80de0.jpg",
+    photo:
+      "https://i.pinimg.com/736x/2f/02/59/2f02592195355b96dc35480cf6a80de0.jpg",
     link: "",
-    tags: ["pwn"]
+    tags: ["pwn"],
   },
   {
     nickname: "rikyiso00",
     name: "Riccardo Isola",
     photo: "/immagini_membri/riccardo_isola.jpg",
     link: "",
-    tags: ["web"]
+    tags: ["web"],
   },
   {
     nickname: "il_vero_gabriele",
     name: "[REDACTED]",
-    photo: "https://static.wikia.nocookie.net/spongebob/images/f/fc/SpongeBob_SquarePants_Theme_Song_%281998%29_01.png",
+    photo:
+      "https://static.wikia.nocookie.net/spongebob/images/f/fc/SpongeBob_SquarePants_Theme_Song_%281998%29_01.png",
     link: "",
-    tags: ["pwn"]
+    tags: ["pwn"],
   },
   {
     nickname: "Pwny",
     name: "Pwny the Horse",
     photo: "/immagini_membri/pwny.jpg",
     link: "https://github.com/WAPEETY/ASCII-horses",
-    tags: ["pwn", "gambling"]
+    tags: ["pwn", "gambling"],
   },
 ];
 
@@ -145,5 +174,5 @@ export const pets = [
     info: "Why does he sit like that? Nobody knows",
     photo: "/img-pets/macchia.jpg",
     tags: ["cat"],
-  }
+  },
 ];
